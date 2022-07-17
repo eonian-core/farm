2b7d7933365223a3c231bb34703fe339126d0c15
solovev <mrbroume@gmail.com>
2022-07-17 11:17:38 +0300

    review fixes p1

--- packages/contracts/contracts/interfaces/ILender.sol
+++ packages/contracts/contracts/interfaces/ILender.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface ILender {
-    function getAvailableCredit() external view returns (uint256);
+    /// @notice Returns the number of tokens the borrower (caller of this function) can take from the lender
+    /// @return Available credit as amount of tokens
+    function availableCredit() external view returns (uint256);
 
-    function getOutstandingDebt() external view returns (uint256);
+    /// @notice Returns the outstanding debt that the borrower (caller of this function) must repay
+    /// @return Outstanding debt as amount of tokens
+    function outstandingDebt() external view returns (uint256);
 
-    function reportPositiveDebtMaintainingResult(uint256 extraFreeFunds)
-        external;
+    /// @notice Reports a positive result of the borrower's debt management.
+    ///         Borrower must call this function if he has made any profit
+    ///         or/and has a free funds available to repay the outstanding debt (if any).
+    /// @param extraFreeFunds an extra amount of free funds borrower's contract has.
+    ///                       This reporting amount must be greater than the borrower's outstanding debt.
+    function reportPositiveDebtManagement(uint256 extraFreeFunds) external;
 
-    function reportNegativeDebtMaintainingResult(uint256 remainingDebt)
-        external;
+    /// @notice Reports a negative result of the borrower's debt management.
+    ///         The borrower must call this function if he is unable to cover his outstanding debt or if he has incurred any losses.
+    /// @param remainingDebt a number of tokens by which the borrower's balance has decreased since the last report.
+    ///                      May include a portion of the outstanding debt that the borrower was unable to repay.
+    function reportNegativeDebtManagement(uint256 remainingDebt) external;
 }
--- packages/contracts/contracts/mixins/Lender.sol
+++ packages/contracts/contracts/mixins/Lender.sol
@@ -20,10 +20,10 @@ abstract contract Lender is ILender, Pausable {
     uint256 constant MAX_BPS = 10_000;
 
     // Amount of tokens that all borrowers have taken
-    uint256 internal _totalDebt = 0;
+    uint256 public totalDebt = 0;
 
     // Debt ratio for the Lender across all borrowers (in BPS, <= 10k)
-    uint256 internal _debtRatio = 0;
+    uint256 public debtRatio = 0;
 
     // Records with information on each borrower using the lender's services
     mapping(address => BorrowerData) public borrowersData;
@@ -36,34 +36,28 @@ abstract contract Lender is ILender, Pausable {
         _;
     }
 
-    /// @notice Returns the number of tokens the borrower (caller of this function) can take from the lender
-    /// @return Available credit as amount of tokens
-    function getAvailableCredit() external view override returns (uint256) {
-        return _getAvailableCredit(msg.sender);
+    /// @inheritdoc ILender
+    function availableCredit() external view override returns (uint256) {
+        return _availableCredit(msg.sender);
     }
 
-    /// @notice Returns the outstanding debt that the borrower (caller of this function) must repay
-    /// @return Outstanding debt as amount of tokens
-    function getOutstandingDebt() external view override returns (uint256) {
-        return _getOutstandingDebt(msg.sender);
+    /// @inheritdoc ILender
+    function outstandingDebt() external view override returns (uint256) {
+        return _outstandingDebt(msg.sender);
     }
 
-    /// @notice Reports a positive result of the borrower's debt management.
-    ///         Borrower must call this function if he has made any profit
-    ///         or/and has a free funds available to repay the outstanding debt (if any).
-    /// @param extraFreeFunds an extra amount of free funds borrower's contract has.
-    ///                       This reporting amount must be greater than the borrower's outstanding debt.
-    function reportPositiveDebtMaintainingResult(uint256 extraFreeFunds)
+    /// @inheritdoc ILender
+    function reportPositiveDebtManagement(uint256 extraFreeFunds)
         external
         override
         onlyBorrowers
     {
         // If the borrower calls this function, it can be assumed that the entire outstanding debt will be repaid
-        uint256 debtPayment = _getOutstandingDebt(msg.sender);
+        uint256 debtPayment = _outstandingDebt(msg.sender);
 
         // Checking whether the borrower is telling the truth about his available funds
         require(
-            _getBorrowerFreeAssets(msg.sender) >= extraFreeFunds + debtPayment
+            _borrowerFreeAssets(msg.sender) >= extraFreeFunds + debtPayment
         );
 
         // TODO: Assess n' pay management fees here
@@ -71,24 +65,21 @@ abstract contract Lender is ILender, Pausable {
         _rebalanceBorrowerFunds(msg.sender, debtPayment, extraFreeFunds);
     }
 
-    /// @notice Reports a negative result of the borrower's debt management.
-    ///         The borrower must call this function if he is unable to cover his outstanding debt or if he has incurred any losses.
-    /// @param remainingDebt a number of tokens by which the borrower's balance has decreased since the last report.
-    ///                      May include a portion of the outstanding debt that the borrower was unable to repay.
-    function reportNegativeDebtMaintainingResult(uint256 remainingDebt)
+    /// @inheritdoc ILender
+    function reportNegativeDebtManagement(uint256 remainingDebt)
         external
         override
         onlyBorrowers
     {
-        uint256 outstandingDebt = _getOutstandingDebt(msg.sender);
+        uint256 borrowerOutstandingDebt = _outstandingDebt(msg.sender);
 
         // Reported "remaining" debt may be greater than outstanding debt if the borrower incurs losses that he cannot cover
-        uint256 debtPayment = remainingDebt <= outstandingDebt
-            ? outstandingDebt - remainingDebt
+        uint256 debtPayment = remainingDebt <= borrowerOutstandingDebt
+            ? borrowerOutstandingDebt - remainingDebt
             : 0;
 
         // Checking whether the borrower has available funds for debt payment
-        require(_getBorrowerFreeAssets(msg.sender) >= debtPayment);
+        require(_borrowerFreeAssets(msg.sender) >= debtPayment);
 
         if (remainingDebt > 0) {
             _decreaseBorrowerCredibility(msg.sender, remainingDebt);
@@ -96,8 +87,8 @@ abstract contract Lender is ILender, Pausable {
 
         // Recalculate the outstanding debt after the ratio is reduced
         // TODO: [Compare gas consumption] Return ratio delta from "_decreaseBorrowerCredibility" and multiple by declared "outstandingDebt"
-        outstandingDebt = _getOutstandingDebt(msg.sender);
-        debtPayment = Math.min(debtPayment, outstandingDebt);
+        borrowerOutstandingDebt = _outstandingDebt(msg.sender);
+        debtPayment = Math.min(debtPayment, borrowerOutstandingDebt);
 
         _rebalanceBorrowerFunds(msg.sender, debtPayment, 0);
     }
@@ -112,42 +103,42 @@ abstract contract Lender is ILender, Pausable {
         uint256 borrowerFreeFunds
     ) internal {
         // Calculate the amount of credit the lender can provide to the borrower (if any)
-        uint256 availableCredit = _getAvailableCredit(borrower);
+        uint256 borrowerAvailableCredit = _availableCredit(borrower);
 
         // Take into account repaid debt, if any
         if (debtPayment > 0) {
             borrowersData[borrower].debt -= debtPayment;
-            _totalDebt -= debtPayment;
+            totalDebt -= debtPayment;
         }
 
         // Allocate some funds to the borrower if possible
-        if (availableCredit > 0) {
-            borrowersData[borrower].debt += availableCredit;
-            _totalDebt += availableCredit;
+        if (borrowerAvailableCredit > 0) {
+            borrowersData[borrower].debt += borrowerAvailableCredit;
+            totalDebt += borrowerAvailableCredit;
         }
 
         // Now we need to compare the allocated funds to the borrower and his current free balance.
         // If the number of unrealized tokens on the borrower's contract is less than the available credit, the lender must give that difference to the borrower.
         // Otherwise (if the amount of the borrower's available funds is greater than he should have according to his share), the lender must take that portion of the funds for himself.
         uint256 freeBorrowerBalance = borrowerFreeFunds + debtPayment;
-        if (freeBorrowerBalance < availableCredit) {
+        if (freeBorrowerBalance < borrowerAvailableCredit) {
             _transferFundsToBorrower(
                 borrower,
-                availableCredit - freeBorrowerBalance
+                borrowerAvailableCredit - freeBorrowerBalance
             );
-        } else if (freeBorrowerBalance > availableCredit) {
+        } else if (freeBorrowerBalance > borrowerAvailableCredit) {
             _takeFundsFromBorrower(
                 borrower,
-                freeBorrowerBalance - availableCredit
+                freeBorrowerBalance - borrowerAvailableCredit
             );
         }
     }
 
     /// @notice Returns the unrealized amount of the lender's tokens (lender's contract balance)
-    function _getFreeAssets() internal view virtual returns (uint256);
+    function _freeAssets() internal view virtual returns (uint256);
 
     /// @notice Returns the unrealized amount of the borrower's tokens (contract balance of the specified borrower)
-    function _getBorrowerFreeAssets(address borrower)
+    function _borrowerFreeAssets(address borrower)
         internal
         view
         virtual
@@ -164,13 +155,13 @@ abstract contract Lender is ILender, Pausable {
         virtual;
 
     /// @notice Returns the total amount of all tokens (including those on the contract balance and taken by borrowers)
-    function _getTotalAssets() internal view returns (uint256) {
-        return _getFreeAssets() + _totalDebt;
+    function _totalAssets() internal view returns (uint256) {
+        return _freeAssets() + totalDebt;
     }
 
     /// @notice Returns the total number of tokens borrowers can take
-    function _getDebtLimit() private view returns (uint256) {
-        return (_debtRatio * _getTotalAssets()) / MAX_BPS;
+    function _debtLimit() private view returns (uint256) {
+        return (debtRatio * _totalAssets()) / MAX_BPS;
     }
 
     /// @notice Lowers the borrower's debt he can take by specified loss and decreases his credibility
@@ -180,27 +171,27 @@ abstract contract Lender is ILender, Pausable {
         uint256 debt = borrowersData[borrower].debt;
 
         // Make sure the borrower's loss is less than his entire debt
-        require(debt > loss);
+        require(debt >= loss);
 
         // To decrease credibility of the borrower we should lower his "debtRatio"
-        if (_debtRatio > 0) {
+        if (debtRatio > 0) {
             uint256 debtRatioChange = Math.min(
-                (_debtRatio * loss) / _totalDebt,
+                (debtRatio * loss) / totalDebt,
                 borrowersData[borrower].debtRatio
             );
             if (debtRatioChange != 0) {
                 borrowersData[borrower].debtRatio -= debtRatioChange;
-                _debtRatio -= debtRatioChange;
+                debtRatio -= debtRatioChange;
             }
         }
 
         // Also, need to reduce the max amount of funds that can be taken by the borrower
-        borrowersData[borrower].debt -= debt - loss;
-        _totalDebt -= loss;
+        borrowersData[borrower].debt -= loss;
+        totalDebt -= loss;
     }
 
     /// @notice See external implementation
-    function _getAvailableCredit(address borrower)
+    function _availableCredit(address borrower)
         internal
         view
         returns (uint256)
@@ -210,50 +201,50 @@ abstract contract Lender is ILender, Pausable {
             return 0;
         }
 
-        uint256 debtLimit = _getDebtLimit();
-        uint256 totalDebt = _totalDebt;
-        uint256 lenderAvailableFunds = debtLimit - totalDebt;
+        uint256 lenderDebtLimit = _debtLimit();
+        uint256 lenderDebt = totalDebt;
+        uint256 borrowerDebtLimit = (borrowersData[borrower].debtRatio *
+            _totalAssets()) / MAX_BPS;
+        uint256 borrowerDebt = borrowersData[borrower].debt;
 
-        // No more funds for credit
-        if (lenderAvailableFunds <= 0) {
+        // There're no more funds for the borrower because he has outstanding debt or the lender's available funds have been exhausted
+        if (
+            lenderDebtLimit <= lenderDebt || borrowerDebtLimit <= borrowerDebt
+        ) {
             return 0;
         }
 
-        uint256 borrowerDebtLimit = (borrowersData[borrower].debtRatio *
-            _getTotalAssets()) / MAX_BPS;
-        uint256 borrowerDebt = borrowersData[borrower].debt;
+        uint256 lenderAvailableFunds = lenderDebtLimit - lenderDebt;
         uint256 borrowerIntendedCredit = borrowerDebtLimit - borrowerDebt;
 
-        // Borrower's debt limit has been exhausted
-        if (borrowerIntendedCredit <= 0) {
-            return 0;
-        }
-
         // Borrower may not take more funds than the lender's limit
-        uint256 availableCredit = Math.min(
+        uint256 borrowerAvailableCredit = Math.min(
             lenderAvailableFunds,
             borrowerIntendedCredit
         );
 
         // Available credit is limited by the existing number of tokens on the lender's contract
-        availableCredit = Math.min(availableCredit, _getFreeAssets());
+        borrowerAvailableCredit = Math.min(
+            borrowerAvailableCredit,
+            _freeAssets()
+        );
 
-        return availableCredit;
+        return borrowerAvailableCredit;
     }
 
     /// @notice See external implementation
-    function _getOutstandingDebt(address borrower)
+    function _outstandingDebt(address borrower)
         internal
         view
         returns (uint256)
     {
         uint256 borrowerDebt = borrowersData[borrower].debt;
-        if (paused() || _debtRatio == 0) {
+        if (paused() || debtRatio == 0) {
             return borrowerDebt;
         }
 
         uint256 borrowerDebtLimit = (borrowersData[borrower].debtRatio *
-            _getTotalAssets()) / MAX_BPS;
+            _totalAssets()) / MAX_BPS;
         if (borrowerDebt <= borrowerDebtLimit) {
             return 0;
         }
