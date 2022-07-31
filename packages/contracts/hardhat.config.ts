d493daabfd4fbeca7b2a8f190f936979515e63ba
solovev <mrbroume@gmail.com>
2022-07-31 22:26:40 +0300

    Hardhat + foundry

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 
+import fs from "fs";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
@@ -10,6 +11,7 @@ import "solidity-coverage";
 import "hardhat-tracer";
 import "hardhat-deploy";
 import "hardhat-docgen";
+import "hardhat-preprocessor";
 
 dotenv.config();
 
@@ -26,7 +28,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
+const config: HardhatUserConfig & { preprocess: any } = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
@@ -42,6 +44,39 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  paths: {
+    sources: "./src",
+    tests: "./test",
+    cache: "./cache_hardhat",
+    artifacts: "./artifacts",
+  },
+  preprocess: {
+    eachLine: () => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          const remappings = getRemappings();
+          const importPartsRegExp = /(.+)"(.+)"/g;
+          const [, prefix, path] = importPartsRegExp.exec(line) ?? [];
+          for (const [find, replace] of remappings) {
+            if (!path.startsWith(find)) {
+              continue;
+            }
+            line = `${prefix} "${replace + path.slice(find.length)}";`;
+            break;
+          }
+        }
+        return line;
+      },
+    }),
+  },
 };
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
 
 export default config;
