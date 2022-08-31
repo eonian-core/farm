8ee40c00468064ffd3f2bba3599b0981fe75abb2
leovs09 <leovs010@gmail.com>
2022-08-31 22:50:22 +0200

    use ERC4626 interface instead of unused function

--- packages/contracts/src/IVault.sol
+++ packages/contracts/src/IVault.sol
@@ -2,14 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "./lending/ILender.sol";
+import "./tokens/IERC4626.sol";
 
-interface IVault is ILender {
+interface IVault is ILender, IERC4626 {
     /// @notice Returns the current version of this vault contract
     /// @return a version in semantic versioning format
     function version() external pure returns (string memory);
-
-    /// @notice Returns the contract address of the ERC20-compliant underlying vault asset.
-    function underlyingAsset() external view returns (address);
 
     /// @notice Revokes a strategy from the vault.
     ///         Sets strategy's dept ratio to zero, so that the strategy cannot take funds from the vault.
