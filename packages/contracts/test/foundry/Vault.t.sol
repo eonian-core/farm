1a81db1cb6a1606250b235b7b669d103603df358
solovev <mrbroume@gmail.com>
2022-08-31 20:27:24 +0300

    fix tests

--- packages/contracts/test/foundry/Vault.t.sol
+++ packages/contracts/test/foundry/Vault.t.sol
@@ -140,7 +140,9 @@ contract VaultTest is Test {
         address strategyAddress = address(strategy);
         vault.addStrategy(strategyAddress, MAX_BPS);
 
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        vm.expectRevert(
+            abi.encodeWithSelector(AccessDeniedForCaller.selector, culprit)
+        );
 
         vm.prank(culprit);
         vault.revokeStrategy(strategyAddress);
