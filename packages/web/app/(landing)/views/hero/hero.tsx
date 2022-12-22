57ddea1f839c486fe5c1f9c5ba6542f0d1769dc7
leovs09 <leovs010@gmail.com>
2022-12-23 00:56:40 +0100

    add mobile support for hero view

--- packages/web/app/(landing)/views/hero/hero.tsx
+++ packages/web/app/(landing)/views/hero/hero.tsx
@@ -2,14 +2,14 @@ import { Roboto } from '@next/font/google'
 import Container from '../contrainer/container'
 import styles from './hero.module.scss'
 
-const roboto = Roboto({ subsets: ['latin'] , weight: ['500', '900']})
+const roboto = Roboto({ subsets: ['latin'] , weight: ['300', '400', '700', '900']})
 
 export default function Hero() {
   return (
     <Container>
       <div className={styles.hero}>
         <h1 className={roboto.className}>
-          Make Crypto <br /> Work for You.
+          <span>Make</span> Crypto <br /> Work <span>for You.</span>
         </h1>
         <p className={roboto.className}>
           We build passive crypto-investment strategies that cares about your money.
