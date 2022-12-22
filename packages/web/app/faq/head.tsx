89088bbed6d8619fd549c418f0bd343bc70e5e79
leovs09 <leovs010@gmail.com>
2022-12-22 22:32:03 +0100

    fix FAQ page layout

--- packages/web/app/faq/head.tsx
+++ packages/web/app/faq/head.tsx
@@ -1,8 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Eonian | FAQ</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Common questions and answers about Eonian, DeFi and crypto" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
