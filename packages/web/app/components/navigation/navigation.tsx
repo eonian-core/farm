b970b154f1fa537dd5dac220cdcc964c3c6ef06d
solovev <mrbroume@gmail.com>
2022-12-24 12:54:29 +0400

    Add EonianLogo component

--- packages/web/app/components/navigation/navigation.tsx
+++ packages/web/app/components/navigation/navigation.tsx
@@ -1,17 +1,23 @@
-import { Roboto } from '@next/font/google'
-import styles from './navigation.module.scss'
-import Link from 'next/link'
+import { Roboto } from "@next/font/google";
+import styles from "./navigation.module.scss";
+import Link from "next/link";
+import EonianLogo from "../logo/logo";
 
-const roboto = Roboto({ subsets: ['latin'] , weight: ['500']})
+const roboto = Roboto({ subsets: ["latin"], weight: ["500"] });
 
 export default function Navigation() {
-    return (
-        <nav className={styles.navigation}>
-            <ul className={styles.content}>
-                <li className={styles.logo}>
-                    <Link href="/" className={roboto.className}><span>Eonian</span></Link>
-                </li>
-            </ul>            
-        </nav>
-    )
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.content}>
+        <li className={styles.logo}>
+          <Link href="/" className="flex flex-row items-center">
+            <div className="mr-3 flex-shrink-0">
+              <EonianLogo height={30} width={70} />
+            </div>
+            <span className={roboto.className}>Eonian</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
