1466e345c8423ac2bef2f897fd7e45ab10cc14d9
sergey.bartosik <wer32dr@gmail.com>
2022-12-25 11:12:35 -0600

    small refactoring

--- packages/web/app/components/collapse/collapse.tsx
+++ packages/web/app/components/collapse/collapse.tsx
@@ -1,13 +1,22 @@
 import { Inter } from "@next/font/google";
+import styles from './collapse.module.scss'
 
-export default function Collapse({ index, label, description }: { index: number, label: string, description: string }) {
+interface CollapseProp {
+  index: number,
+  label: string,
+  description: string
+}
+
+export default function Collapse({ index, label, description }: CollapseProp) {
   return (
-    <div tabIndex={index} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-      <div className="collapse-title text-xl font-medium">
-        {label}
-      </div>
-      <div className="collapse-content">
-        <p>{description}</p>
+    <div className={styles.container}>
+      <div tabIndex={index} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          {label}
+        </div>
+        <div className="collapse-content">
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
