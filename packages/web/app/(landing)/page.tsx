248f4a1ed310d9e04bf7896a2e2418b69324f0b0
leovs09 <leovs010@gmail.com>
2022-12-26 23:38:47 +0100

    add mobile support for solution block

--- packages/web/app/(landing)/page.tsx
+++ packages/web/app/(landing)/page.tsx
@@ -4,16 +4,18 @@ import { MDXProvider } from '@mdx-js/react'
 import type { MDXComponents } from 'mdx/types'
 import dynamic from 'next/dynamic'
 import styles from './page.module.css'
-import Card from '../components/card/card'
 import Hero from './views/hero/hero'
 import heading from '../components/heading/heading'
+import Solution, {Block, BigNumber} from './views/solution/solution'
 
 const components: MDXComponents = {
   h2: heading.H2,
   Hero,
   Problem: dynamic(import('./views/problem/problem')),
   Category: dynamic(import('../components/category/category')),
-  Solution: dynamic(import('./views/solution/solution')),
+  Solution,
+  Block,
+  BigNumber,
   Card: dynamic(import('../components/card/card')),
 }
 
--- packages/web/app/(landing)/views/solution/solution.tsx
+++ packages/web/app/(landing)/views/solution/solution.tsx
@@ -4,6 +4,7 @@ import styles from './solution.module.scss'
 // Props for the Solution component
 interface SolutionProps {
     children: React.ReactNode
+
 }
 
 export const Solution = ({children}: SolutionProps) => {
@@ -19,3 +20,32 @@ export const Solution = ({children}: SolutionProps) => {
 }
 
 export default Solution;
+
+// props for Block component
+interface BlockProps {
+    children: React.ReactNode
+    /** Align number to the end of the block and reverse on small screen sizes*/
+    end?: boolean
+}
+
+export const Block = ({children, end}: BlockProps) => {
+    return (
+        <div className={`${styles.block} ${end ? styles.blockEnd : ''}`}>
+            {children}
+        </div>
+    )
+}
+
+// props for BigNumber component
+interface BigNumberProps {
+    children: React.ReactNode
+}
+
+
+export const BigNumber = ({children}: BigNumberProps) => {
+    return (
+        <div className={styles.bigNumber}>
+            <span>{children}</span>
+        </div>
+    )
+}
