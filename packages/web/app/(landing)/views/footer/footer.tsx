758ff7f4ecf6a165fcc45ff8a295b3f3f9a8cf01
solovev <mrbroume@gmail.com>
2022-12-27 16:57:30 +0400

    review fixes

--- packages/web/app/(landing)/views/footer/footer.tsx
+++ packages/web/app/(landing)/views/footer/footer.tsx
@@ -1,92 +1,31 @@
 import Link from "next/link";
 import React from "react";
-import IconDiscord from "../../../components/icons/icon-discord";
-import IconGithub from "../../../components/icons/icon-github";
-import IconMedium from "../../../components/icons/icon-medium";
-import IconTelegram from "../../../components/icons/icon-telegram";
-import IconTwitter from "../../../components/icons/icon-twitter";
 import LogoWithText from "../../../components/logo/logo-with-text";
-
-const getLinks = (
-  prefix: string
-): { key: string; name: string; href: string }[] => {
-  return Object.keys(process.env)
-    .filter((key) => key.startsWith(prefix))
-    .map((key) => {
-      const value = process.env[key] ?? "";
-      const [name, href] = value.split("@");
-      return {
-        key: key.substring(prefix.length + 1).toLowerCase(),
-        name,
-        href,
-      };
-    });
-};
-
-const getPages = () => getLinks("NAVIGATION");
-
-interface MultiLangLink {
-  name: string;
-  hrefs: Record<string, string>;
-  icon?: React.ReactNode;
-}
-
-const getSocials = () => {
-  const icons: Record<string, React.ReactNode> = {
-    discord: <IconDiscord />,
-    twitter: <IconTwitter />,
-    github: <IconGithub />,
-    telegram: <IconTelegram />,
-    medium: <IconMedium />,
-  };
-  return getLinks("SOCIAL").reduce((result, { key: rawKey, name, href }) => {
-    const keyParts = rawKey.split("_");
-    const key = keyParts[0];
-    const language = keyParts[1]?.toLowerCase() ?? "en";
-
-    if (!result[key]) {
-      result[key] = {
-        icon: icons[key],
-        name,
-        hrefs: {
-          [language]: href,
-        },
-      };
-    } else {
-      result[key].hrefs[language] = href;
-    }
-    return result;
-  }, {} as Record<string, MultiLangLink>);
-};
+import socials from "./socials";
 
 const FooterLink = (props: {
   name: string;
-  href: string;
+  href?: string;
+  hrefs?: Record<string, string>;
   icon?: React.ReactNode;
 }) => {
-  const { name, href, icon } = props;
-  return (
-    <li key={name} className="flex items-center text-slate-300">
-      {icon && <div className="translate-y-px">{icon}</div>}
-      <Link className="ml-1 leading-7 hover:underline" href={href}>
-        {name}
-      </Link>
-    </li>
-  );
-};
-
-const FooterMultiLangLink = ({ name, hrefs, icon }: MultiLangLink) => {
-  const langauges = Object.keys(hrefs);
+  const { name, href, hrefs, icon } = props;
+  const links = hrefs ? hrefs : { en: href ?? "" };
+  const langauges = Object.keys(links);
   return (
     <li key={name} className="flex items-center text-slate-300">
       {icon && <div className="translate-y-px">{icon}</div>}
       {langauges.map((langauge, index) => {
-        const href = hrefs[langauge];
+        const href = links[langauge];
         const lang = langauge.toUpperCase();
         const total = langauges.length;
         return (
           <React.Fragment key={langauge}>
-            <Link className="ml-1 leading-7 hover:underline" href={href}>
+            <Link
+              className="ml-1 leading-7 hover:underline"
+              target={href.startsWith("/") ? "_self" : "_blank"}
+              href={href}
+            >
               {total > 1 ? (index === 0 ? `${name} ${lang}` : lang) : name}
             </Link>
             {index < total - 1 ? "," : null}
@@ -98,27 +37,26 @@ const FooterMultiLangLink = ({ name, hrefs, icon }: MultiLangLink) => {
 };
 
 const Footer = () => {
-  const pages = React.useMemo(getPages, []);
-  const socials = React.useMemo(getSocials, []);
-
   return (
     <div className="w-full max-w-screen-lg p-8">
-      <div className="flex justify-around">
-        <div>
+      <div className="flex flex-wrap justify-center md:justify-around">
+        <div className="p-4">
           <LogoWithText />
           <div className="mt-4">Hand-crafted with ❤️ by our team</div>
         </div>
-        <div>
-          <h5 className="mb-2 text-xl font-medium leading-normal">Resources</h5>
-          <ul>
-            {pages.map(({ name, href }) => (
-              <FooterLink key={name} name={name} href={href} />
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h5 className="mb-2 text-xl font-medium leading-normal">Social</h5>
-          {Object.values(socials).map(FooterMultiLangLink)}
+        <div className="flex flex-wrap p-4 md:justify-around">
+          <div className="mx-12 mb-6">
+            <h5 className="mb-2 text-xl font-medium leading-normal">
+              Resources
+            </h5>
+            <ul>
+              <FooterLink name="FAQ" href="/faq" />
+            </ul>
+          </div>
+          <div className="mx-12 mb-6">
+            <h5 className="mb-2 text-xl font-medium leading-normal">Social</h5>
+            {socials.map(FooterLink)}
+          </div>
         </div>
       </div>
       <div className="mt-12 text-center">
