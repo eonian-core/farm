555dce65c030e0f14dbc3ae7af8b3a87bbe741f1
leovs09 <leovs010@gmail.com>
2022-12-27 23:32:37 +0100

    open external links in new tab

---
+++ packages/web/next.config.mjs
@@ -0,0 +1,35 @@
+import rehypeExternalLinks from 'rehype-external-links'
+import createMDX from "@next/mdx";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    scrollRestoration: true,
+  },
+  
+  // Adds mdx support
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    
+    rehypePlugins: [[
+      rehypeExternalLinks, {
+       target: '_blank', // open in new tab
+       rel: 'noopener noreferrer' // prenect tabnabbing <https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/>
+      }]],
+    
+      // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+})
+
+export default withMDX(nextConfig)
+
