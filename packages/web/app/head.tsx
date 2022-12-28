1917d7e171ce1d7e867283d70931cee6707df332
leovs09 <leovs010@gmail.com>
2022-12-28 02:08:52 +0100

    add favicons

--- packages/web/app/head.tsx
+++ packages/web/app/head.tsx
@@ -1,10 +1,18 @@
 export default function Head() {
   return (
     <>
-      <title>Eonian</title>
+      <title>Eonian | Most safe crypto yield aggregator</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Passive investment crypto yield aggregator" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Decentralised and secure protocol for passive investments with peace in mind." />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="theme-color" content="#181b1b"></meta>
     </>
   )
 }
