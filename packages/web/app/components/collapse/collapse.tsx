5f52c8d1b1f66c38b98c7cd57f79b53e45eb2576
sergey.bartosik <wer32dr@gmail.com>
2023-01-01 10:04:50 -0600

    migrated to container structure

--- packages/web/app/components/collapse/collapse.tsx
+++ packages/web/app/components/collapse/collapse.tsx
@@ -1,19 +1,19 @@
-import { Inter } from "@next/font/google";
-import styles from './collapse.module.scss'
+import styles from "./collapse.module.scss";
 
 interface CollapseProp {
-  index: number,
-  label: string,
-  description: string
+  index: number;
+  label: string;
+  description: string;
 }
 
 export default function Collapse({ index, label, description }: CollapseProp) {
   return (
     <div className={styles.container}>
-      <div tabIndex={index} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-        <div className="collapse-title text-xl font-medium">
-          {label}
-        </div>
+      <div
+        tabIndex={index}
+        className="collapse-arrow rounded-box collapse border border-base-300 bg-base-100"
+      >
+        <div className="collapse-title text-xl font-medium">{label}</div>
         <div className="collapse-content">
           <p>{description}</p>
         </div>
