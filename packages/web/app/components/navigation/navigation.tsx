4cefc98aa7d912ad5a0363840e0654f18c5b023c
leovs09 <leovs010@gmail.com>
2023-01-06 00:10:40 +0100

    add desktop navigation

--- packages/web/app/components/navigation/navigation.tsx
+++ packages/web/app/components/navigation/navigation.tsx
@@ -1,14 +1,27 @@
 import styles from "./navigation.module.scss";
 import LogoWithText from "../logo/logo-with-text";
+import { InternalLink } from "../links/links";
 
 export default function Navigation() {
+  const showCommunity = process.env.NEXT_PUBLIC_FEATURE_COMMUNITY_PAGE === 'true';
+  const showFooter = process.env.NEXT_PUBLIC_FEATURE_FAQ_PAGE === 'true';
+  
+
   return (
     <nav className={styles.navigation}>
-      <ul className={styles.content}>
-        <li className={styles.logo}>
-          <LogoWithText />
-        </li>
-      </ul>
+      <div className={styles.content}>
+        <div className={styles.logo}><LogoWithText /></div>
+        <ul className={styles.list}>
+          <li>
+            {showCommunity && <InternalLink href="/community">Community</InternalLink>}
+          </li>
+
+          <li>
+            {showFooter && <InternalLink href="/faq">FAQ</InternalLink>}
+          </li>
+        </ul>
+
+      </div>
     </nav>
   );
 }
