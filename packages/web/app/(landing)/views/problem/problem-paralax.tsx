99ee929f925c8476c16c94ea7a58598ac11093ac
leovs09 <leovs010@gmail.com>
2023-01-13 03:13:46 +0100

    add different animation speed for different images

--- packages/web/app/(landing)/views/problem/problem-paralax.tsx
+++ packages/web/app/(landing)/views/problem/problem-paralax.tsx
@@ -7,35 +7,35 @@ export function ProblemParalax(){
     const { width: w, height: h } = useThree((state) => state.viewport)
 
     return <>
-        <Block offset={0} factor={0.3}>
+        <Block offset={0} factor={0.3} speed={0.03}>
             <ThreeImage
                 scale={[w / 5, w / 5]}
                 position={[-7, -4, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
-        <Block offset={0} factor={0.2}>
+        <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
                 scale={[w / 7, w / 7]}
                 position={[0, 3, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
-        <Block offset={0} factor={0.2}>
+        <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
                 scale={[w / 6, w / 6]}
                 position={[8, -1, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
-        <Block offset={0} factor={0.1}>
+        <Block offset={0} factor={0.1} speed={0.015}>
             <ThreeImage
                 scale={[w / 9, w / 9]}
                 position={[5, -6, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
-        <Block offset={0} factor={0.1}>
+        <Block offset={0} factor={0.1} speed={0.01}>
             <ThreeImage
                 scale={[w / 11, w / 11]}
                 position={[-4, -7, 0]}
