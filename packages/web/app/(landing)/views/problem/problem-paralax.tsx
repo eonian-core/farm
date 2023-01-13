ed7692563f06cc428ef09bdcbba5a1f1ffc1e42a
leovs09 <leovs010@gmail.com>
2023-01-13 02:46:51 +0100

    distrribute images on background

--- packages/web/app/(landing)/views/problem/problem-paralax.tsx
+++ packages/web/app/(landing)/views/problem/problem-paralax.tsx
@@ -9,7 +9,28 @@ export function ProblemParalax(){
     return <Block offset={0} factor={0.3}>
         <ThreeImage
             scale={[w / 5, w / 5]}
-            position={[-6, -4.5, 0]}
+            position={[-6, -4, 0]}
+            url="/assets/bitcoin_in_crystal.png"
+        />
+        <ThreeImage
+            scale={[w / 7, w / 7]}
+            position={[0, 1, 0]}
+            url="/assets/bitcoin_in_crystal.png"
+        />
+        <ThreeImage
+            scale={[w / 6, w / 6]}
+            position={[8, -2, 0]}
+            url="/assets/bitcoin_in_crystal.png"
+        />
+        <ThreeImage
+            scale={[w / 9, w / 9]}
+            position={[3, -10, 0]}
+            url="/assets/bitcoin_in_crystal.png"
+        />
+
+        <ThreeImage
+            scale={[w / 11, w / 11]}
+            position={[-4, -9, 0]}
             url="/assets/bitcoin_in_crystal.png"
         />
     </Block>
