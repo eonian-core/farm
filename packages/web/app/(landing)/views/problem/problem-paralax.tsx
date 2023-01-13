92a213d96400bab070679f280b032733cb32f980
leovs09 <leovs010@gmail.com>
2023-01-13 03:26:25 +0100

    fix paralax offset

--- packages/web/app/(landing)/views/problem/problem-paralax.tsx
+++ packages/web/app/(landing)/views/problem/problem-paralax.tsx
@@ -10,35 +10,35 @@ export function ProblemParalax(){
         <Block offset={0} factor={0.3} speed={0.03}>
             <ThreeImage
                 scale={[w / 5, w / 5]}
-                position={[-7, -4, 0]}
+                position={[-7, 2, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
         <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
                 scale={[w / 7, w / 7]}
-                position={[0, 3, 0]}
+                position={[0, 9, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
         <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
                 scale={[w / 6, w / 6]}
-                position={[8, -1, 0]}
+                position={[8, 5, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
         <Block offset={0} factor={0.1} speed={0.015}>
             <ThreeImage
                 scale={[w / 9, w / 9]}
-                position={[5, -6, 0]}
+                position={[5, 0, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
         <Block offset={0} factor={0.1} speed={0.01}>
             <ThreeImage
                 scale={[w / 11, w / 11]}
-                position={[-4, -7, 0]}
+                position={[-4, -1, 0]}
                 url="/assets/bitcoin_in_crystal.png"
             />
         </Block>
