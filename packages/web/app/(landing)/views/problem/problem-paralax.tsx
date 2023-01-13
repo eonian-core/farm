bd04542ca74608092200e6b11dec27ac7301120f
leovs09 <leovs010@gmail.com>
2023-01-13 03:07:56 +0100

    use different spped factors for different images

--- packages/web/app/(landing)/views/problem/problem-paralax.tsx
+++ packages/web/app/(landing)/views/problem/problem-paralax.tsx
@@ -5,35 +5,45 @@ import { Block } from "../../../components/paralax/block";
 
 export function ProblemParalax(){
     const { width: w, height: h } = useThree((state) => state.viewport)
-    
-    return <Block offset={0} factor={0.3}>
-        <ThreeImage
-            scale={[w / 5, w / 5]}
-            position={[-7, -4, 0]}
-            url="/assets/bitcoin_in_crystal.png"
-        />
-        <ThreeImage
-            scale={[w / 7, w / 7]}
-            position={[0, 1, 0]}
-            url="/assets/bitcoin_in_crystal.png"
-        />
-        <ThreeImage
-            scale={[w / 6, w / 6]}
-            position={[8, -2, 0]}
-            url="/assets/bitcoin_in_crystal.png"
-        />
-        <ThreeImage
-            scale={[w / 9, w / 9]}
-            position={[3, -10, 0]}
-            url="/assets/bitcoin_in_crystal.png"
-        />
 
-        <ThreeImage
-            scale={[w / 11, w / 11]}
-            position={[-4, -9, 0]}
-            url="/assets/bitcoin_in_crystal.png"
-        />
-    </Block>
+    return <>
+        <Block offset={0} factor={0.3}>
+            <ThreeImage
+                scale={[w / 5, w / 5]}
+                position={[-7, -4, 0]}
+                url="/assets/bitcoin_in_crystal.png"
+            />
+        </Block>
+        <Block offset={0} factor={0.2}>
+            <ThreeImage
+                scale={[w / 7, w / 7]}
+                position={[0, 3, 0]}
+                url="/assets/bitcoin_in_crystal.png"
+            />
+        </Block>
+        <Block offset={0} factor={0.2}>
+            <ThreeImage
+                scale={[w / 6, w / 6]}
+                position={[8, -1, 0]}
+                url="/assets/bitcoin_in_crystal.png"
+            />
+        </Block>
+        <Block offset={0} factor={0.1}>
+            <ThreeImage
+                scale={[w / 9, w / 9]}
+                position={[5, -6, 0]}
+                url="/assets/bitcoin_in_crystal.png"
+            />
+        </Block>
+        <Block offset={0} factor={0.1}>
+            <ThreeImage
+                scale={[w / 11, w / 11]}
+                position={[-4, -7, 0]}
+                url="/assets/bitcoin_in_crystal.png"
+            />
+        </Block>
+    </>
+    
 }
 
 export default ProblemParalax
