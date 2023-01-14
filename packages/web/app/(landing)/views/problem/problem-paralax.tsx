e66092540985a3ed70afb5418a97896053703d99
leovs09 <leovs010@gmail.com>
2023-01-14 04:43:32 +0100

    add glow effect

--- packages/web/app/(landing)/views/problem/problem-paralax.tsx
+++ packages/web/app/(landing)/views/problem/problem-paralax.tsx
@@ -1,5 +1,6 @@
 import { Billboard, Image as ThreeImage, MeshReflectorMaterial, Sparkles } from '@react-three/drei'
 import { useThree } from "@react-three/fiber";
+import { Bloom, EffectComposer, Outline } from '@react-three/postprocessing';
 import { Block } from "../../../components/paralax/block";
 
 export function ProblemParalax(){
@@ -10,32 +11,33 @@ export function ProblemParalax(){
         <Block offset={0} factor={0.3} speed={0.03}>
             
             <ThreeImage
-                scale={[w / 5, w / 5]}
+                scale={[w / 6, w / 6]}
                 position={[-7, 2, 0]}
                 url="/assets/bitcoin_in_crystal.png"
-                opacity={0.9}
+                opacity={0.95}
+                toneMapped={false}
             >
-                <Sparks orbit={0.8} amount={30} color={'#2bf1f1'} />
+                <Sparks orbit={0.8} amount={30} color={'#195757'} />
             </ThreeImage>
         </Block>
         <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
-                scale={[w / 8, w / 8]}
+                scale={[w / 9, w / 9]}
                 position={[0, 7.5, 0]}
                 url="/assets/etherium_crystal_2.png"
-                opacity={0.8}
+                opacity={0.9}
             >
-                <Sparks orbit={0.9} color={'#cf45db'} />
+                <Sparks orbit={0.9} color={'#551b5a'} amount={30} />
             </ThreeImage>
         </Block>
         <Block offset={0} factor={0.2} speed={0.02}>
             <ThreeImage
-                scale={[w / 7, w / 7]}
+                scale={[w / 9, w / 9]}
                 position={[8, 5, 0]}
                 url="/assets/crystal_3.png"
                 opacity={0.8}
             >
-                <Sparks orbit={0.9} color={'#b539d4'} />
+                <Sparks orbit={0.9} color={'#551e63'} amount={30} />
             </ThreeImage>
         </Block>
         <Block offset={0} factor={0.1} speed={0.015}>
@@ -43,9 +45,9 @@ export function ProblemParalax(){
                 scale={[w / 9, w / 9]}
                 position={[5, 0, 0]}
                 url="/assets/crystal_5.png"
-                opacity={0.7}
+                opacity={0.9}
             >
-                <Sparks orbit={0.8} amount={30} color={'#ebc90c'} />
+                <Sparks orbit={0.8} amount={30} color={'#504815'} />
             </ThreeImage>
         </Block>
         <Block offset={0} factor={0.1} speed={0.01}>
@@ -55,9 +57,12 @@ export function ProblemParalax(){
                 url="/assets/crystal_4.png"
                 opacity={0.6}
             >
-                <Sparks orbit={0.8} amount={30} color={'#e7d741'} />
+                <Sparks orbit={0.8} amount={30} color={'#615b1a'} />
             </ThreeImage>
         </Block>
+        <EffectComposer>
+            <Bloom mipmapBlur luminanceThreshold={0.2} luminanceSmoothing={0.3} height={1} intensity={0.5} radius={0.8} />
+      </EffectComposer>
     </>
     
 }
