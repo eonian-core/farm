856c9c25dceeaa5a07ff9149489bf403e80cb5e9
leovs09 <leovs010@gmail.com>
2023-01-15 04:22:24 +0100

    add more images with gradients

--- packages/web/app/(landing)/views/problem/problem-parallax.tsx
+++ packages/web/app/(landing)/views/problem/problem-parallax.tsx
@@ -12,6 +12,7 @@ import {
 } from "framer-motion";
 import { ParalaxContainer } from "../../../components/parallax/parallax-container";
 import { ParallaxBlock } from "../../../components/parallax/parallax-block";
+import clsx from "clsx";
 
 export function ProblemParallax() {
   const ref = React.useRef<HTMLDivElement>(null);
@@ -27,50 +28,46 @@ export function ProblemParallax() {
         y={0.45}
         scale={0.2}
       >
-        
-        <div className={styles.imageContainer}>
+        <div className={clsx(styles.imageContainer, styles.bitcoinImageContainer)}>
           <div className={styles.graidentWrapper}>
             <div className={styles.gradientBitcoin1}></div>
             <div className={styles.gradientBitcoin2}></div>
             <div className={styles.gradientBitcoin3}></div>
           </div>
           <Image fill src={"/assets/bitcoin_in_crystal.png"} alt="Bitcoin crystal" />
-      </div>
+        </div>
       </ParallaxBlock>
-      {/* <FloatingCrystal
-        src="/assets/etherium_crystal_2.png"
-        x={0.5}
-        y={-0.05}
-        scale={0.75}
-        fade={0.1}
-        animationProgress={scrollYProgress}
-      />
-      <FloatingCrystal
-        src="/assets/crystal_3.png"
-        x={1.15}
+
+      <ParallaxBlock
+        x={1}
         y={0.2}
-        scale={0.65}
-        fade={0.1}
-        animationProgress={scrollYProgress}
-      />
-      <FloatingCrystal
-        src="/assets/crystal_5.png"
-        x={0.9}
-        y={1}
-        scale={0.65}
-        fade={0.1}
-        animationProgress={scrollYProgress}
-        // blur={0.5}
-      />
-      <FloatingCrystal
-        src="/assets/crystal_4.png"
-        x={0.2}
-        y={1.1}
-        scale={0.65}
-        fade={0.1}
-        animationProgress={scrollYProgress}
-        // blur={0.5}
-      /> */}
+        scale={0.1}
+      >
+        <div className={clsx(styles.imageContainer, styles.etheriumImageContainer)}>
+          <div className={styles.graidentWrapper}>
+            <div className={styles.gradientEtherium1}></div>
+            <div className={styles.gradientEtherium2}></div>
+          </div>
+          <Image fill src={"/assets/etherium_crystal_2.png"} alt="Etherium crystal" />
+        </div>
+      </ParallaxBlock>
+
+      <ParallaxBlock
+        x={0.6}
+        y={0.9}
+        scale={0.09}
+      >
+        <div className={clsx(styles.imageContainer, styles.crystalPiramidImageContainer)}>
+          <div className={styles.graidentWrapper}>
+            <div className={styles.gradientPiramid1}></div>
+            <div className={styles.gradientPiramid2}></div>
+            <div className={styles.gradientPiramid3}></div>
+            <div className={styles.gradientPiramid4}></div>
+          </div>
+          <Image fill src={"/assets/crystal_4.png"} alt="Crystal" />
+        </div>
+      </ParallaxBlock>
+
     </ParalaxContainer>
   );
 }
