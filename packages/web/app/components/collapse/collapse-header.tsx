69a23e4d335bc5703c24041dbb7deca8939c53ee
leovs09 <leovs010@gmail.com>
2023-01-16 23:42:38 +0100

    close/open collapse only on click to header

--- packages/web/app/components/collapse/collapse-header.tsx
+++ packages/web/app/components/collapse/collapse-header.tsx
@@ -1,11 +1,15 @@
+import { useCollapseContext } from "./collapse";
 
 // props
 interface ColalapseHeaderProps {
     children?: React.ReactNode;
-}   
+}
 
-export const ColalapseHeader = ({children}: ColalapseHeaderProps) => (
-    <h3 className="collapse-title text-xl font-medium">{children}</h3>
-)
+export const ColalapseHeader = ({ children }: ColalapseHeaderProps) => {
+    const { onHeaderClick } = useCollapseContext();
+    return (
+        <h3 className="collapse-title text-xl font-medium" onClick={onHeaderClick}>{children}</h3>
+    )
+}
 
 export default ColalapseHeader;
--- packages/web/app/components/collapse/collapse.tsx
+++ packages/web/app/components/collapse/collapse.tsx
@@ -3,18 +3,31 @@ import React from "react";
 import clsx from "clsx";
 
 interface CollapseProp {
+  /** Need for correct tab order */
   index: number;
   children: React.ReactNode;
   /** Set open at start */
   open: boolean;
 }
 
+export interface CollapseContextState {
+  isOpen: boolean;
+  onHeaderClick: () => void
+}
+
+const CollapseContext = React.createContext<CollapseContextState>({
+  isOpen: false,
+  onHeaderClick: () => {}
+});
+
+export const useCollapseContext = () => React.useContext(CollapseContext);
+
 export default function Collapse({ index = 0, children, open }: CollapseProp) {
   const [isOpen, setIsOpen] = React.useState(open);
   const toggleOpen = React.useCallback(() => setIsOpen(!isOpen), [isOpen]);
 
   return (
-    <div className={styles.container} onClick={toggleOpen}>
+    <div className={styles.container}>
       <div
         tabIndex={index}
         className={clsx("collapse-arrow rounded-box collapse border border-base-300 bg-base-100", {
@@ -22,7 +35,9 @@ export default function Collapse({ index = 0, children, open }: CollapseProp) {
           'collapse-close': !isOpen
         })}
       >
-        {children}
+        <CollapseContext.Provider value={{ isOpen, onHeaderClick: toggleOpen }}>
+          {children}
+        </CollapseContext.Provider>
       </div>
     </div>
   );
