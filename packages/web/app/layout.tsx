35f5f02d3fcd4304875b26d024ea577c162db04e
leovs09 <leovs010@gmail.com>
2023-01-17 01:53:59 +0100

    use lazy loading for google analytics

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { Roboto } from "@next/font/google";
-import { useCallback, useState, WheelEventHandler } from "react";
+import { useState } from "react";
 import clsx from "clsx";
-import { GoogleAnalytics } from "nextjs-google-analytics"; // TODO: use lazy loading
+import dynamic from 'next/dynamic'
 
 import './globals.scss'
 import './tailwind.css'
@@ -13,8 +13,8 @@ import SlidingFooter from "./components/sliding-footer/sliding-footer";
 import Footer from "./components/footer/footer";
 import styles from './layout.module.scss'
 
-const roboto = Roboto({ 
-  subsets: ['latin', 'cyrillic'], 
+const roboto = Roboto({
+  subsets: ['latin', 'cyrillic'],
   weight: ['300', '400', '500', '700', '900'],
   display: 'block' // force to show font anyway
 })
@@ -25,7 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [isMenuOpen, setMenuState] = useState(false);
-    
+
+  const GoogleAnalytics = dynamic(
+    () => import('nextjs-google-analytics')
+      .then(({ GoogleAnalytics }) => GoogleAnalytics));
+
   const locale = 'en';
   return (
     <html lang={locale}>
@@ -36,10 +40,10 @@ export default function RootLayout({
       */}
       <head />
 
-      <body className={clsx(roboto.className, {[styles.menuOpen]: isMenuOpen})}>
+      <body className={clsx(roboto.className, { [styles.menuOpen]: isMenuOpen })}>
 
-        <Navigation onStateChange={setMenuState}/>
-        
+        <Navigation onStateChange={setMenuState} />
+
         <SlidingFooter footer={<Footer locale={locale} />}>
           {children}
         </SlidingFooter>
