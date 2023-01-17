c3309e1126edcbb042061d6363fea744598b08a9
leovs09 <leovs010@gmail.com>
2023-01-17 01:48:47 +0100

    add story for logo with text

---
+++ packages/web/app/components/logo/logo-with-text.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import LogoWithText from './logo-with-text';
+
+const meta: Meta<typeof LogoWithText> = {
+    title: 'Components/LogoWithText',
+    component: LogoWithText,
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <div className="story-wrapper">
+                <Story />
+                <style global jsx>{`
+                .story-wrapper {
+                    width: 100%;
+                    height: 100%;
+                
+                    max-width: var(--max-width);
+                    padding: 0 var(--width-gap);
+                }
+          `}</style>
+            </div>
+        ),
+    ],
+}
+
+export default meta;
+type Story = StoryObj<typeof LogoWithText>;
+
+export const Default: Story = {
+    args: {
+    }
+}
