9a20556845545c11c883df1634d581906967b09e
leovs09 <leovs010@gmail.com>
2023-01-21 02:23:36 +0100

    add ssr for problem section images

--- packages/web/app/(landing)/views/problem/problem.tsx
+++ packages/web/app/(landing)/views/problem/problem.tsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic"
 import Container from "../../../components/contrainer/container"
 import styles from './problem.module.scss'
 
-// Props for the Problem component
+
 interface ProblemProps {
     /** content of the block, expected to have: Category, h2 and p elements */
     children: React.ReactNode
@@ -13,7 +13,7 @@ interface ProblemProps {
  * @param children - content of the block
  */
 export const Problem = ({children}: ProblemProps) => {
-    const ProblemParallax = dynamic(import('./problem-parallax'), { ssr: false })
+    const ProblemParallax = dynamic(import('./problem-parallax'))
     
     return (
         <Container className={styles.pageContainer}>
