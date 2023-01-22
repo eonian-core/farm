5f3a0bc15c96dfbbda23db4c9eee8b9c32c6f783
solovev <mrbroume@gmail.com>
2023-01-22 18:28:25 +0400

    avatars

--- packages/web/app/(landing)/views/founders/founder.tsx
+++ packages/web/app/(landing)/views/founders/founder.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconPerson from "../../../components/icons/icon-person";
 import styles from "./founder.module.scss";
+import Image from "next/image";
 
 interface Props {
   avatar: string;
@@ -11,7 +12,11 @@ const Founder: React.FC<Props> = ({ children, avatar }) => {
   return (
     <li className={styles.container}>
       <div className={styles.avatar}>
-        <IconPerson width="100%" height="100%" />
+        {avatar ? (
+          <Image src={avatar} alt="avatar" fill />
+        ) : (
+          <IconPerson width="100%" height="100%" />
+        )}
       </div>
       {children}
     </li>
