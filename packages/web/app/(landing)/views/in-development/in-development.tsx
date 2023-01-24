db794d1354b9cc9213d2865f562268d0087cae84
leovs09 <leovs010@gmail.com>
2023-01-24 01:09:08 +0100

    add abstract background image

--- packages/web/app/(landing)/views/in-development/in-development.tsx
+++ packages/web/app/(landing)/views/in-development/in-development.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+
 import Container from "../../../components/contrainer/container";
 import styles from './in-development.module.scss';
+import Neon64Pic from './assets/neon-46.png'
 
 interface ContainerProps {
     /** content of the block, expected to have: h2, p  and Card elements */
@@ -9,11 +12,16 @@ interface ContainerProps {
 export const InDevelopment = ({children}: ContainerProps) => {
     return (
         <Container className={styles.pageContainer}>
-            <div className={styles.inDevelopment}>
-                {children}
+            <div className={styles.inDevelopmentWrapper}>
+                <div className={styles.inDevelopment}>
+                    {children}
+                </div>
+                <div className={styles.backgroundImage}>
+                    <Image src={Neon64Pic} alt="Abstract neon cicrle" placeholder="blur" />
+                </div>
             </div>
         </Container>
     );
-    };
+};
 
 export default InDevelopment;
