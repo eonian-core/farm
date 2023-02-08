53705c05765b651d7746a30cc17dcda0601ec9cf
sergey.bartosik <wer32dr@gmail.com>
2023-02-07 21:11:52 -0600

    added deep-security section changed sec-ops layout

--- packages/web/app/security/views/deepsecurity/deepsecurity.tsx
+++ packages/web/app/security/views/deepsecurity/deepsecurity.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import styles from "./deepsecurity.module.scss";
-import Image from "next/image";
-import robotPic from "../../assets/robot.png";
-import Column from "../../columns/columns";
 
 export interface DeepSecurityProps {
   children: React.ReactNode;
@@ -12,11 +9,8 @@ export default function DeepSecurity({
   children,
 }: DeepSecurityProps) {
   return (
-    <>
-      <div className={styles.painting}>
-        <Image className={styles.robotImage} src={robotPic} alt="robot protecting vault" placeholder="blur" />
+      <div className={styles.deepSecurity}>
+        {children}
       </div>
-      <Column>{children}</Column>
-    </>
   );
 }
