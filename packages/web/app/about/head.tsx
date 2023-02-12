18938c68627577fe37a1f9f9cdf238c6b8e8a640
leovs09 <leovs010@gmail.com>
2023-02-12 01:12:39 +0100

    add SEO heading to all pages

---
+++ packages/web/app/about/head.tsx
@@ -0,0 +1,8 @@
+import DefaultHead from '../head'
+
+export default function Head() {
+  return <DefaultHead seo={{
+    title: "Eonian | About",
+    description: "Eonian Protocol is a decentralized protocol that allows users to earn interest on their crypto assets",
+  }}/>
+}
---
+++ packages/web/app/community/head.tsx
@@ -0,0 +1,8 @@
+import DefaultHead from '../head'
+
+export default function Head() {
+  return <DefaultHead seo={{
+    title: "Eonian | Community",
+    description: "Join the Eonian growing community and make your contribution to the future of investments!",
+  }}/>
+}
