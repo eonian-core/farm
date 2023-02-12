0aa7ad5333ced5a9a4c236f797d0558098b54511
leovs09 <leovs010@gmail.com>
2023-02-12 23:53:25 +0100

    add anchor link handle for faq page

--- packages/web/app/components/collapse/collapse-header.tsx
+++ packages/web/app/components/collapse/collapse-header.tsx
@@ -1,15 +1,38 @@
+import { useEffect, useMemo } from "react";
 import { useCollapseContext } from "./collapse";
+import { toId } from "./to-id";
 
 // props
 interface ColalapseHeaderProps {
     children?: React.ReactNode;
 }
 
+/** Collapse header provide id, that can be used for anchor link */
 export const ColalapseHeader = ({ children }: ColalapseHeaderProps) => {
-    const { onHeaderClick } = useCollapseContext();
+    const { onHeaderClick, id: collapseId, setCollapseId } = useCollapseContext();
+    const id = useToId(children?.toString());
+
+    // set id if not set
+    useEffect(() => {
+        if (typeof id === 'string' && id !== collapseId) {
+            setCollapseId(id);
+        }
+    }, [collapseId, id, setCollapseId]);
+    
+
     return (
-        <h3 className="collapse-title text-xl font-medium" onClick={onHeaderClick}>{children}</h3>
+        <h3
+            className="collapse-title text-xl font-medium"
+            onClick={onHeaderClick}
+            id={id}
+        >{children}</h3>
     )
 }
 
 export default ColalapseHeader;
+
+/** Momoized verion of to id */
+export const useToId = (slug?: string) => {
+    return useMemo(() => toId(slug), [slug]);
+}
+
--- packages/web/app/components/collapse/collapse.tsx
+++ packages/web/app/components/collapse/collapse.tsx
@@ -1,6 +1,8 @@
 import styles from "./collapse.module.scss";
-import React from "react";
+import React, { RefObject, useCallback, useEffect, useRef, useState } from "react";
 import clsx from "clsx";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useLocationHash } from "./use-location-hash";
 
 interface CollapseProp {
   /** Need for correct tab order */
@@ -13,21 +15,47 @@ interface CollapseProp {
 export interface CollapseContextState {
   isOpen: boolean;
   onHeaderClick: () => void
+  /** Id which can be used by anchor link to navigate to collapse */
+  id: string | null;
+  /** Allow header to say which id belongs to the collapse */
+  setCollapseId: (id: string) => void;
 }
 
 const CollapseContext = React.createContext<CollapseContextState>({
   isOpen: false,
-  onHeaderClick: () => {}
+  onHeaderClick: () => {},
+  id: null,
+  setCollapseId: () => {},
 });
 
 export const useCollapseContext = () => React.useContext(CollapseContext);
 
 export default function Collapse({ index = 0, children, open }: CollapseProp) {
-  const [isOpen, setIsOpen] = React.useState(open);
-  const toggleOpen = React.useCallback(() => setIsOpen(!isOpen), [isOpen]);
+  const { isOpen, setIsOpen, toggleOpen } = useIsOpen(open);
+
+  const [id, setId] = useState<string | null>(null)
+  const hash = useLocationHash()
+
+  // for scrollIntoView
+  const ref = useRef<HTMLDivElement>(null);
+  
+  // open collapse if hash matches id
+  // react only when hash or id changes
+  useEffect(() => {
+    if(typeof hash !== 'string' || typeof id !== 'string') 
+      return
+
+    if (id === hash) {
+      setIsOpen(true)
+      scrollIntoView(ref)
+    }
+    
+    // important to react only when hash or id changes
+    // prevent accidential opening or scroll on re-render
+  }, [hash, id])
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div
         tabIndex={index}
         className={clsx("collapse-arrow rounded-box collapse border border-base-300 bg-base-100", {
@@ -35,10 +63,23 @@ export default function Collapse({ index = 0, children, open }: CollapseProp) {
           'collapse-close': !isOpen
         })}
       >
-        <CollapseContext.Provider value={{ isOpen, onHeaderClick: toggleOpen }}>
+        <CollapseContext.Provider value={{ 
+            isOpen, onHeaderClick: toggleOpen, 
+            id, setCollapseId: setId 
+            }} >
           {children}
         </CollapseContext.Provider>
       </div>
     </div>
   );
+}
+
+const useIsOpen = (open: boolean) => {
+  const [isOpen, setIsOpen] = useState(open);
+  const toggleOpen = useCallback(() => setIsOpen(!isOpen), [isOpen]);
+  return { isOpen, setIsOpen, toggleOpen };
+}
+
+export const scrollIntoView = (ref: RefObject<HTMLDivElement>) => {
+  ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
 }
