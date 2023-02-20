e551ac1cb604bb3bda8e0c852450d5775cb6fabe
siarhei.bartoshyk <wer32dr@gmail.com>
2023-02-20 13:28:46 -0600

    added background image for deep security view

--- packages/web/app/security/views/deepsecurity/deepsecurity.tsx
+++ packages/web/app/security/views/deepsecurity/deepsecurity.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./deepsecurity.module.scss";
+import Image from "next/image";
+import electronicCircuitPic from "../../assets/electronics-circuit.png";
 
 export interface DeepSecurityProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function DeepSecurity({
-  children,
-}: DeepSecurityProps) {
-  return (
-      <div className={styles.deepSecurity}>
-        {children}
-      </div>
-  );
+                                         children,
+                                     }: DeepSecurityProps) {
+    return (
+        <div className={styles.deepSecurity}>
+            {children}
+            <div className={styles.imageContainer}>
+                <Image src={electronicCircuitPic} alt="Rocket starting in cityscape of future city" placeholder="blur"/>
+            </div>
+        </div>
+    );
 }
