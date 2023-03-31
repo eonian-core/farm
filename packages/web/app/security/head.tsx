84793a4863c2895c9c5b1926a295b4fc23cde38e
leovs09 <leovs010@gmail.com>
2023-03-31 18:46:36 +0200

    📝 docs(head.tsx): update SEO title and description The SEO title and description have been updated to better reflect the content of the page. The new title is "Eonain | Security" and the new description is "Eonain Security measures overview". This change improves the SEO of the page and makes it more accurate.

--- packages/web/app/security/head.tsx
+++ packages/web/app/security/head.tsx
@@ -2,7 +2,7 @@ import DefaultHead from '../head'
 
 export default function Head() {
   return <DefaultHead seo={{
-    title: "Eonain FAQ | Security",
-    description: "Eonain Security overview",
+    title: "Eonain | Security",
+    description: "Eonain Security measures overview",
   }}/>
 }
