d2e92969730488f42117c2de90e4225010879fb3
solovev <mrbroume@gmail.com>
2023-04-08 19:43:34 +0400

    fix all pages

--- packages/web/app/mission/page.tsx
+++ packages/web/app/mission/page.tsx
@@ -2,32 +2,36 @@
 
 import { MDXProvider } from "@mdx-js/react";
 import type { MDXComponents } from "mdx/types";
-import dynamic from "next/dynamic";
 import styles from "./page.module.scss";
 import heading from "../components/heading/heading";
 import { LinkInText } from "../components/links/link-in-text";
+import Card, { Target } from "../components/card/card";
+import Revolution from "./views/revolution/revolution";
+import Solution from "./views/solution/solution";
+import blockquote from "../components/category/category";
+import Problem from "./views/problem/problem";
+import Name from "./views/who-are-we/name";
+import Description from "./views/who-are-we/description";
+import WhoWeAre from "./views/who-are-we/who-are-we";
+import Content from "./content/en.mdx";
 
 const components: MDXComponents = {
   h1: heading.H1,
   h2: heading.H2,
-  a: LinkInText as any, // no lazy loading for links, prevent flash of content
-  WhoWeAre: dynamic(import("./views/who-are-we/who-are-we")),
-  Description: dynamic(import("./views/who-are-we/description")),
-  Name: dynamic(import("./views/who-are-we/name")),
-  Problem: dynamic(import("./views/problem/problem")),
-  blockquote: dynamic(import("../components/category/category")) as any,
-  Solution: dynamic(import("./views/solution/solution")),
-  Revolution: dynamic(import("./views/revolution/revolution")),
-  Card: dynamic(import("../components/card/card")),
-  Target: dynamic(
-    import("../components/card/card").then(({ Target }) => Target)
-  ),
-}
+  a: LinkInText as any,
+  WhoWeAre,
+  Description,
+  Name,
+  Problem,
+  blockquote: blockquote as any,
+  Solution,
+  Revolution,
+  Card,
+  Target,
+};
 
 
 export default function Home() {
-  const Content = dynamic(import(`./content/en.mdx`));
-
   return (
     <main className={styles.main}>
       <div className={styles.content}>
--- packages/web/app/security/page.tsx
+++ packages/web/app/security/page.tsx
@@ -6,34 +6,47 @@ import dynamic from "next/dynamic";
 import styles from "./page.module.scss";
 import heading from "../components/heading/heading";
 import {InternalLink} from "../components/links/links";
+import Column from "./columns/columns";
+import ImageCard, { Target } from "../components/image-card/image-card";
+import Card from "../components/card/card";
+import Approach from "./views/approach/approach";
+import SecOps from "./views/secops/secops";
+import SecOpsDescription from "./views/secops/secops-description";
+import SecOpsCards from "./views/secops/secops-cards";
+import SecOpsStatusIndicator from "./views/secops/secops-status-indicator";
+import SecOpsCodeReview from "./views/secops/secops-code-review";
+import SecOpsRiskThreatModel from "./views/secops/secops-risk-threat-model";
+import blockquote from "./views/secops/secops-indicator-label";
+import strong from "./views/secops/secops-indicator-label-green";
+import DeepSecurity from "./views/deepsecurity/deepsecurity";
+import DeepSecurityDescription from "./views/deepsecurity/deepsecurity-description";
+import Content from "./content/en.mdx";
 
 const components: MDXComponents = {
   h1: heading.H1,
   h2: heading.H2,
   h3: heading.H3,
   h4: heading.H4,
-  Column: dynamic(import('./columns/columns')),
-  ImageCard: dynamic(import('../components/image-card/image-card')),
-  Card: dynamic(import('../components/card/card')),
-  Target: dynamic(import("../components/image-card/image-card").then(({Target}) => Target)),
-  InternalLink: dynamic(import('../components/links/links').then(({ InternalLink }) => InternalLink)),
-  Approach: dynamic(import('./views/approach/approach')),
-  SecOps: dynamic(import('./views/secops/secops')),
-  SecOpsDescription: dynamic(import('./views/secops/secops-description')),
-  SecOpsCards: dynamic(import('./views/secops/secops-cards')),
-  SecOpsStatusIndicator: dynamic(import('./views/secops/secops-status-indicator')),
-  SecOpsCodeReview: dynamic(import('./views/secops/secops-code-review')),
-  SecOpsRiskThreatModel: dynamic(import('./views/secops/secops-risk-threat-model')),
-  blockquote: dynamic(import('./views/secops/secops-indicator-label')) as any,
-  strong: dynamic(import('./views/secops/secops-indicator-label-green')) as any,
-  DeepSecurity: dynamic(import('./views/deepsecurity/deepsecurity')),
-  DeepSecurityDescription: dynamic(import('./views/deepsecurity/deepsecurity-description')),
-}
+  Column,
+  ImageCard,
+  Card,
+  Target,
+  InternalLink,
+  Approach,
+  SecOps,
+  SecOpsDescription,
+  SecOpsCards,
+  SecOpsStatusIndicator,
+  SecOpsCodeReview,
+  SecOpsRiskThreatModel,
+  blockquote: blockquote as any,
+  strong: strong as any,
+  DeepSecurity,
+  DeepSecurityDescription,
+};
 
 
 export default function Home() {
-  const Content = dynamic(import(`./content/en.mdx`));
-
   return (
     <main className={styles.main}>
       <div className={styles.content}>
