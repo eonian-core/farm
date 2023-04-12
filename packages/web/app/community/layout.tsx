5b0c4aaf34ff0c28ef214437f70eb1f015d9057b
Sergey <mrbroume@gmail.com>
2023-04-12 08:24:37 +0400

    Apply suggestions from code review
    Co-authored-by: Vladislav Goncharov <LeoVS010@gmail.com>

--- packages/web/app/community/layout.tsx
+++ packages/web/app/community/layout.tsx
@@ -4,6 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return children;
 }
 
+// Cannot work with `use client`, in our case can be placed only in `layout.tsx`
 export const metadata: Metadata = {
   title: "Community",
   description:
--- packages/web/app/mission/layout.tsx
+++ packages/web/app/mission/layout.tsx
@@ -5,6 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return children;
 }
 
+// Cannot work with `use client`, in our case can be placed only in `layout.tsx`
 export const metadata: Metadata = {
   title: "Mission",
   description:
