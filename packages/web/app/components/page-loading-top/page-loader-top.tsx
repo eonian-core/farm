2bc6a059495e0ad0d137c15f0ffb369b3ec99485
solovev <mrbroume@gmail.com>
2023-04-23 10:24:37 +0400

    refactor(page-loader-top.tsx): simplify animation initialization and add support for page transition context feat(page-loader-top.tsx): add support for page transition context to control animation start and stop

--- packages/web/app/components/page-loading-top/page-loader-top.tsx
+++ packages/web/app/components/page-loading-top/page-loader-top.tsx
@@ -2,9 +2,16 @@
 
 import React from "react";
 import styles from "./page-loader-top.module.scss";
+import { usePathname, useSearchParams } from "next/navigation";
+import { usePageTransitionContext } from "../../store/page-transition-context";
 
 const PageLoaderTop = () => {
   const ref = React.useRef<HTMLDivElement>(null);
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [pageLoading] = usePageTransitionContext();
+
   const [animation, setAnimation] = React.useState<Animation | null>(null);
 
   React.useEffect(() => {
@@ -12,21 +19,35 @@ const PageLoaderTop = () => {
     if (!loader) {
       return;
     }
-    const loaderProgress = [
-      { width: "0", opacity: 0.5 },
-      { width: "20vw", opacity: 0.9, offset: 0.1 },
-      { width: "40vw", opacity: 1, offset: 0.3 },
-      { width: "60vw", opacity: 0.9, offset: 0.5 },
-      { width: "100vw", opacity: 0.75 },
-    ];
-    const animation = loader.animate(loaderProgress, {
-      duration: 5000,
-      fill: "forwards",
-    });
-    animation.cancel();
-    setAnimation(animation);
-  }, []);
 
+    if (!animation) {
+      const loaderProgress = [
+        { width: "0", opacity: 0.5 },
+        { width: "20vw", opacity: 0.9, offset: 0.1 },
+        { width: "40vw", opacity: 1, offset: 0.3 },
+        { width: "60vw", opacity: 0.9, offset: 0.5 },
+        { width: "100vw", opacity: 0.75 },
+      ];
+      const newAnimation = loader.animate(loaderProgress, {
+        duration: 5000,
+        fill: "forwards",
+      });
+      newAnimation.cancel();
+      return setAnimation(newAnimation);
+    }
+
+    if (!pageLoading) {
+      return;
+    }
+
+    const url = pathname + searchParams.toString();
+    if (!pageLoading || pageLoading === url) {
+      animation?.finish();
+      setTimeout(() => animation.cancel(), 100);
+    } else {
+      animation?.play();
+    }
+  }, [pathname, searchParams, pageLoading, animation]);
 
   return <div ref={ref} className={styles.loader} />;
 };
