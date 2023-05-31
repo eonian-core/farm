b0383943a8eff5524dc4020bfac547ad58251f9e
solovev <mrbroume@gmail.com>
2023-05-31 11:05:10 +0400

    optional segments

---
+++ packages/web/app/earn/[[...symbol]]/page.tsx
@@ -0,0 +1,31 @@
+import styles from "./page.module.scss";
+import Form from "./form/form";
+import { getVaults } from "../../api";
+import { redirect } from "next/navigation";
+
+interface PageParams {
+  params: {
+    symbol: string;
+  };
+}
+
+export async function generateStaticParams(): Promise<
+  PageParams["params"][][]
+> {
+  const { data } = await getVaults({ symbols: true, revalidate: 600 });
+  return data.vaults.map(({ symbol }) => [{ symbol }]);
+}
+
+export default async function Earn({ params }: PageParams) {
+  const { symbol } = params;
+  if (!symbol) {
+    const [[{ symbol }]] = await generateStaticParams();
+    return redirect("/earn/" + symbol);
+  }
+
+  return (
+    <main className={styles.main}>
+      <Form vaultSymbol={symbol} />
+    </main>
+  );
+}
