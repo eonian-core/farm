c97a51b76a2ebdc8ad5e5bfa539314d922e6e865
solovev <mrbroume@gmail.com>
2023-06-06 19:47:55 +0400

    Revert "test"
    This reverts commit dc67a211a418a4b3ab5941d4510a14a666e16e81.

--- packages/web/app/api/apollo.client.ts
+++ packages/web/app/api/apollo.client.ts
@@ -8,26 +8,29 @@ import {
   from,
 } from "@apollo/client";
 import { ErrorResponse, onError } from "@apollo/client/link/error";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 import { scalarTypePolicies } from "./gql/graphql";
 
 function errorHandler({ graphQLErrors, networkError }: ErrorResponse) {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+if (graphQLErrors)
+  graphQLErrors.forEach(({ message, locations, path }) =>
+    console.log(
+      `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+    )
+  );
+if (networkError) console.log(`[Network error]: ${networkError}`);
 }
 
-export const client = new ApolloClient({
-  cache: new InMemoryCache({ typePolicies: scalarTypePolicies }),
-  link: from([
-    onError(errorHandler),
-    new HttpLink({
-      uri: process.env.GRAPH_URL || "http://localhost:4000/",
-    }),
-  ]),
+export const { getClient } = registerApolloClient(() => {
+  return new ApolloClient({
+    cache: new InMemoryCache({ typePolicies: scalarTypePolicies }),
+    link: from([
+      onError(errorHandler),
+      new HttpLink({
+        uri: process.env.GRAPH_URL || "http://localhost:4000/",
+      }),
+    ]),
+  });
 });
 
 export const query = async <
@@ -35,7 +38,7 @@ export const query = async <
   TVariables extends OperationVariables = OperationVariables
 >(
   options: QueryOptions<TVariables, T>
-) => client.query<T, TVariables>(options);
+) => getClient().query<T, TVariables>(options);
 
 export const mutate = async <
   TData = any,
@@ -44,4 +47,4 @@ export const mutate = async <
   TCache extends InMemoryCache = InMemoryCache
 >(
   options: MutationOptions<TData, TVariables, TContext>
-) => client.mutate<TData, TVariables, TContext, TCache>(options);
+) => getClient().mutate<TData, TVariables, TContext, TCache>(options);
--- packages/web/app/earn/[[...symbol]]/page.tsx
+++ packages/web/app/earn/[[...symbol]]/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import styles from "./page.module.scss";
 import Form from "./form/form";
 import { Vault, getVaultBySymbol, getVaults } from "../../api";
-import { redirect, useParams } from "next/navigation";
-import { use } from "react";
+import { redirect } from "next/navigation";
 
 interface PageParams {
   params: {
@@ -17,15 +14,29 @@ async function getSymbols() {
   return data.vaults.map(({ symbol }) => [{ symbol }]);
 }
 
-export default async function Earn() {
-  return <FormWrapper />;
+export async function generateStaticParams() {
+  try {
+    return await getSymbols();
+  } catch (e: unknown) {
+    return [];
+  }
 }
 
-function FormWrapper() {
-  const { symbol: symbols } = useParams();
-  const data = use(getSymbols());
-  console.log(data);
+export default async function Earn({ params }: PageParams) {
+  const { symbol: symbols } = params;
+  if (!symbols) {
+    const [[{ symbol }]] = await getSymbols();
+    return redirect("/earn/" + symbol);
+  }
+
+  const [symbol] = symbols;
+  const {
+    data: { vaultBySymbol: vault },
+  } = await getVaultBySymbol({ symbol });
+
   return (
-    <main className={styles.main}>{/* <Form vault={vault as Vault} /> */}</main>
+    <main className={styles.main}>
+      <Form vault={vault as Vault} />
+    </main>
   );
 }
