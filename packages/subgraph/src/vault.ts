a0e98ca15c982fcbb6db6425faee5baa6e6dbf19
leovs09 <leovs010@gmail.com>
2023-06-11 19:24:24 +0200

    remove logging

--- packages/subgraph/src/vault.ts
+++ packages/subgraph/src/vault.ts
@@ -5,11 +5,8 @@ import {
 } from "../generated/Vault/Vault"
 import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
 
-import { log } from "matchstick-as/assembly/log";
-
 export function handleAdminChanged(event: AdminChangedEvent): void {
   const id = event.transaction.hash.concatI32(event.logIndex.toI32())
-  log.info("handleAdminChanged: " + id.toHexString(), [])
 
   let entity = new AdminChanged(
     id
