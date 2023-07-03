53013bb8afd0b9c41de157987bc1510873e153fc
leovs09 <leovs010@gmail.com>
2023-07-03 18:49:26 +0200

    add test for ownable

--- packages/contracts/test/unit/BaseStrategy.t.sol
+++ packages/contracts/test/unit/BaseStrategy.t.sol
@@ -437,4 +437,16 @@ contract BaseStrategyTest is TestWithERC1820Registry {
 
         assertEq(baseStrategy.minimumBetweenExecutions(), time);
     }
+
+    function testFailWithPermissionSetMinimumTimeBetweenExecutions(uint256 time) public {
+        vm.assume(time > 1000);
+
+        assertEq(baseStrategy.minimumBetweenExecutions(), minReportInterval);
+
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        vm.prank(address(culprit));
+        baseStrategy.setMinimumBetweenExecutions(time);
+
+        assertEq(baseStrategy.minimumBetweenExecutions(), time);
+    }
 }
