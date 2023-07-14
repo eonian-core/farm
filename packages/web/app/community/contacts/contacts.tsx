3e1609fda0ca7c44b9e5dfecfe63b3da6ca89516
solovev <mrbroume@gmail.com>
2023-07-14 20:10:40 +0400

    use nextui instead of radix

--- packages/web/app/community/contacts/contacts.tsx
+++ packages/web/app/community/contacts/contacts.tsx
@@ -1,72 +1,94 @@
 import React from "react";
-import * as Collapsible from '@radix-ui/react-collapsible';
-import styles from './contacts.module.scss';
+import styles from "./contacts.module.scss";
 import IconDownOpen from "../../components/icons/icon-down-open";
-import IconClose from "../../components/icons/icon-close";
 import ExternalLink from "../../components/links/external-link";
-import { SocialLink, useLocalSocials, useOtherLanguageslSocials } from "../../socials";
-
+import {
+  SocialLink,
+  useLocalSocials,
+  useOtherLanguageslSocials,
+} from "../../socials";
+import { Dropdown } from "@nextui-org/react";
 
 export const Contacts = () => {
-    const localLinks = useLocalSocials()
-    const otherLocales = useOtherLanguageslSocials()
+  const localLinks = useLocalSocials();
+  const otherLocales = useOtherLanguageslSocials();
 
-    return (
-        <div className={styles.contacts}>
-            <ul>
-                {localLinks.map(({ name, href, icon }) => (
-                    <li key={name}>
-                        <ExternalLink href={href} icon={icon}>{name}</ExternalLink>
-                    </li>
-                ))}
-            </ul>
+  return (
+    <div className={styles.contacts}>
+      <ul>
+        {localLinks.map(({ name, href, icon }) => (
+          <li key={name}>
+            <ExternalLink href={href} icon={icon}>
+              {name}
+            </ExternalLink>
+          </li>
+        ))}
+      </ul>
 
-            <OtherLanguages otherLocales={otherLocales} />
-        </div>
-    )
-}
+      <OtherLanguages otherLocales={otherLocales} />
+    </div>
+  );
+};
 
 export default Contacts;
 
-export const OtherLanguages = ({ otherLocales }: { otherLocales: Record<string, Array<SocialLink>>}) => (
-    <OtherLanguagesCollapse>
-        <ul className={styles.otherLanguagesHighLevelList}>
-            {Object.keys(otherLocales).map(locale => (
-                <li key={locale}>
-                    <h5>{locale}</h5>
-                    <ul>
-                        {otherLocales[locale].map(({ name, href, icon }) => (
-                            <li key={name}>
-                                <ExternalLink href={href} icon={icon}>{name}</ExternalLink>
-                            </li>
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </ul>
-    </OtherLanguagesCollapse>
-)
+export const OtherLanguages = ({
+  otherLocales,
+}: {
+  otherLocales: Record<string, Array<SocialLink>>;
+}) => (
+  <div className={styles.otherLanguagesCollapse}>
+    <Dropdown placement="bottom">
+      <Dropdown.Trigger>
+        <div className={styles.otherLanguagesTrigger}>
+          <span>Other Languages</span>
 
-export const OtherLanguagesCollapse = ({ children }: { children: React.ReactNode }) => {
-    const [open, setOpen] = React.useState(false);
+          <button className={styles.icon}>
+            <IconDownOpen />
+          </button>
+        </div>
+      </Dropdown.Trigger>
 
-    return (
-        <Collapsible.Root open={open} onOpenChange={setOpen} className={styles.otherLanguagesCollapse}>
-            <Collapsible.Trigger asChild>
-                <div className={styles.otherLanguagesTrigger}>
-                    <span>Other Languages</span>
-                    
-                    <button className={styles.icon}>{
-                        !open ? 
-                            <IconDownOpen /> : 
-                            <IconClose />
-                    }</button>
-                </div>
-            </Collapsible.Trigger>
+      <OtherLanguagesMenu otherLocales={otherLocales} />
+    </Dropdown>
+  </div>
+);
 
-            <Collapsible.Content>
-                {children}
-            </Collapsible.Content>
-        </Collapsible.Root>
-    )
+function OtherLanguagesMenu({
+  otherLocales,
+}: {
+  otherLocales: Record<string, Array<SocialLink>>;
+}) {
+  const options = useOptions(otherLocales);
+  return (
+    <Dropdown.Menu>
+      {options.map(({ name, href, icon }) => (
+        <Dropdown.Item key={name}>
+          <ExternalLink
+            className={styles.otherLanguagesLink}
+            href={href}
+            icon={icon}
+          >
+            {name}
+          </ExternalLink>
+        </Dropdown.Item>
+      ))}
+    </Dropdown.Menu>
+  );
+}
+
+function useOptions(otherLocales: Record<string, Array<SocialLink>>) {
+  const mapLink = (link: SocialLink, locale: string) => ({
+    ...link,
+    name: `${link.name} (${locale.toUpperCase()})`,
+  });
+
+  const callback = () =>
+    Object.keys(otherLocales)
+      .map((locale) =>
+        otherLocales[locale].map((link) => mapLink(link, locale))
+      )
+      .flat();
+
+  return React.useMemo(callback, [otherLocales]);
 }
