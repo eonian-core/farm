6598269690e0724e187fde8a374b729f68113f83
solovev <mrbroume@gmail.com>
2023-08-05 18:22:01 +0400

    Revert "logrocket debug"
    This reverts commit 3afe02b74173a4c36c4e3ba38dbbb0b873fe9414.

--- packages/web/app/earn/components/vault-list.tsx
+++ packages/web/app/earn/components/vault-list.tsx
@@ -30,11 +30,6 @@ export function VaultList({ vaultsByChain }: Props) {
 
   useFetchPositionInfo(chainId, vaults);
 
-  React.useEffect(() => {
-    const a: number[] | null = null;
-    console.log(a!.length + a!.concat([])!.length)
-  }, [])
-
   return (
     <>
       <div className={styles.header}>
