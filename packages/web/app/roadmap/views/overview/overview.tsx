211f24304a2e9751cbc3e631e36c9548a42b2f8c
leovs09 <leovs010@gmail.com>
2023-08-26 00:30:46 +0200

    add fade in component

--- packages/web/app/roadmap/views/overview/overview.tsx
+++ packages/web/app/roadmap/views/overview/overview.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-
 import { PropsWithChildren } from "react";
 import Container from "../../../components/contrainer/container";
-import FadeIn from "../../../components/fade-in/fade-in";
 import styles from './overview.module.scss'
 import rocketPic from './assets/rocket-launch.png'
 import FadeInList from '../../../components/fade-in/fade-in-list';
 import { useIsLaptopOrSmaller } from '../../../components/resize-hooks/screens';
+import { FadeInImage } from '../../../components/fade-in-image/fade-in-image';
 
 
 export const Overview = ({children}: PropsWithChildren) => {
@@ -16,19 +14,18 @@ export const Overview = ({children}: PropsWithChildren) => {
         <Container className={styles.pageContainer}>
             <div className={styles.overview}>
 
-                <FadeIn 
+                <FadeInImage 
                     className={styles.imageContainer}
                     delay={0.1} 
                     amount={0.1} 
                     fadeUpInitial='20%'
-                    >
-                    <Image src={rocketPic} alt="Futuristic launching to orbit" placeholder="blur" />
-                </FadeIn>
+                    src={rocketPic} 
+                    alt="Futuristic launching to orbit"
+                />
 
                 <FadeInList className={styles.content} delay={!isLaptopOrSmaller ? 0.3 : 0.7} amount={0.1}>
                     {children}
                 </FadeInList>
-
                 
             </div>
         </Container>
