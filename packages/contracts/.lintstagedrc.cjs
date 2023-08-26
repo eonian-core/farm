3a5f0fc1af84a46e9acef6c90de731aab8233239
solovev <mrbroume@gmail.com>
2023-08-26 08:51:06 +0400

    lintstaged contracts

--- packages/contracts/.lintstagedrc.cjs
+++ packages/contracts/.lintstagedrc.cjs
@@ -1,5 +1,10 @@
+const { check } = require('./check-license');
+
 module.exports = {
   '*.(ts|tsx)': (filenames) => {
     return filenames.length > 5 ? 'eslint . --ext .ts,.tsx --fix' : `eslint ${filenames.join(' ')} --fix`;
   },
+  '*.sol': (filenames) => {
+    return `node -e 'require(\"./check-license\").check([${filenames.join()}])'`;
+  }
 };
--- packages/web/.lintstagedrc.cjs
+++ packages/web/.lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.(ts|tsx)': (filenames) =>
-    filenames.length > 5 ? 'eslint . --ext .ts,.tsx --fix' : `eslint ${filenames.join(' ')} --fix`,
-  '*.json': ['prettier --write'],
+  '*.(ts|tsx)': (filenames) => {
+    return filenames.length > 5 ? 'eslint . --ext .ts,.tsx --fix' : `eslint ${filenames.join(' ')} --fix`;
+  },
 };
