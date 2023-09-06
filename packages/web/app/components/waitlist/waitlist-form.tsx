6682674e6b380b131ab47bcda9b3cf65241c85e6
leovs09 <leovs010@gmail.com>
2023-09-06 23:29:32 +0200

    add gradient highlight

--- packages/web/app/components/waitlist/waitlist-form.tsx
+++ packages/web/app/components/waitlist/waitlist-form.tsx
@@ -46,6 +46,8 @@ export const WaitlistForm = ({ onSubmit }: WaitlistFormProps) => {
                         Join Waitlist
                     </button>
                 </div>
+
+                
         </form>
     )
 }
