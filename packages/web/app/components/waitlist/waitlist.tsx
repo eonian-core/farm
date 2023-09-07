981883b8cbcb00f10423231475c52550fe6b9a30
leovs09 <leovs010@gmail.com>
2023-09-07 23:50:29 +0200

    add delay before switch to dashboard card

--- packages/web/app/components/waitlist/waitlist.tsx
+++ packages/web/app/components/waitlist/waitlist.tsx
@@ -1,22 +1,33 @@
 import Card from "../card/card";
 import { useWaitlist } from "../../providers/waitlist";
 import { WaitlistForm } from "./waitlist-form";
+import { useCallback, useState } from "react";
 
 export interface WaitlistProps {
     /** content of the block, expected to have: h2, p for Card which will be shown after user joined */
     children: React.ReactNode
 }
 
-export const WaitList = ({ children }: WaitlistProps) => {
+const SHOW_DASHBOARD_BUTTON_DELAY = 3000
 
+export const WaitList = ({ children }: WaitlistProps) => {
+    const [isJustSubmited, setIsJustSubmited] = useState(false)
     const {join, isJoined, openDashboard} = useWaitlist()
 
-    if(isJoined) 
+    const joinAndTrack = useCallback(async (email: string) => {
+        await join(email)
+        setIsJustSubmited(true)
+
+        setTimeout(() => setIsJustSubmited(false), SHOW_DASHBOARD_BUTTON_DELAY);
+    }, [join, setIsJustSubmited])
+
+    if(isJoined && !isJustSubmited) 
         return (
             <Card onClick={openDashboard}>
                 {children}
             </Card>
         )
 
-    return <WaitlistForm onSubmit={join} />
+    return <WaitlistForm onSubmit={joinAndTrack} />
 }
+
