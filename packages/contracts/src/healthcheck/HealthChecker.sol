a4f9bdae0ede804c52ce111529aca7c29c7f31f1
solovev <mrbroume@gmail.com>
2023-09-19 08:54:42 +0400

    fixes

--- packages/contracts/src/healthcheck/HealthChecker.sol
+++ packages/contracts/src/healthcheck/HealthChecker.sol
@@ -24,7 +24,7 @@ abstract contract HealthChecker is SafeInitializable, OwnableUpgradeable {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 
     // ------------------------------------------ Constructors ------------------------------------------
 
