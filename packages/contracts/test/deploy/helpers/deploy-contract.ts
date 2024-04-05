ae50bb2f51f21ec3719a1086d6ec74b5a24fa702
Solovyev Sergey <mrbroume@gmail.com>
2024-04-05 10:04:29 +0400

    transactions test

--- packages/contracts/test/deploy/helpers/deploy-contract.ts
+++ packages/contracts/test/deploy/helpers/deploy-contract.ts
@@ -1,6 +1,6 @@
 import type { ContractName, HardhatRuntimeEnvironment } from 'hardhat/types'
 import { type DeployResult } from '../../../hardhat/deployment/plugins/Deployer'
 
-export async function deployContract(contractName: ContractName, args: unknown[], hre: HardhatRuntimeEnvironment): Promise<DeployResult> {
-  return await hre.deploy(contractName, null, args)
+export async function deployContract(contractName: ContractName, args: unknown[], hre: HardhatRuntimeEnvironment, id: string | null = null): Promise<DeployResult> {
+  return await hre.deploy(contractName, id, args)
 }
