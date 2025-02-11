34dd40b669f379f96bcf3c4c3cf091cbbd8833e8
Solovyev Sergey <mrbroume@gmail.com>
2025-02-11 10:07:04 +0400

    deploy-erc1820 task

--- packages/contracts/hardhat/tasks/index.ts
+++ packages/contracts/hardhat/tasks/index.ts
@@ -2,6 +2,7 @@ import './deploy'
 import './deploy-error-catcher'
 import './transfer-ownership'
 import './xfi'
+import './deploy-erc1820'
 
 export * from './accounts'
 export * from './start-hardhat-node'
