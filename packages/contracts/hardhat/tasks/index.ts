45499fdb26b6d0fe9df5faf0186906b67b1a95cd
Solovyev Sergey <mrbroume@gmail.com>
2025-03-02 21:16:36 +0400

    Contract verification task

--- packages/contracts/hardhat/tasks/index.ts
+++ packages/contracts/hardhat/tasks/index.ts
@@ -2,6 +2,7 @@ import './deploy'
 import './deploy-error-catcher'
 import './transfer-ownership'
 import './adjust-vault-debt-ratio'
+import './verify-implementations'
 
 export * from './accounts'
 export * from './start-hardhat-node'
